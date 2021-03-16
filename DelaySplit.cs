75c3cec7626138e1ba01d05223edf49b8b4325b1
sg4e <46875746+sg4e@users.noreply.github.com>
2021-03-16 00:32:48 -0400

    Initial commit

---
+++ DelaySplit.cs
@@ -0,0 +1,113 @@
+﻿using LiveSplit.Model;
+using LiveSplit.UI;
+using LiveSplit.UI.Components;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LiveSplit.DelaySplit
+{
+    public class DelaySplit : IComponent
+    {
+        private LiveSplitState State;
+        private TimerModel Model;
+        private CancellationTokenSource LastCancellationToken;
+        private Form Form;
+
+        public DelaySplit(LiveSplitState state)
+        {
+            ContextMenuControls = new Dictionary<string, Action>();
+            this.State = state;
+            Model = new TimerModel();
+            Model.CurrentState = state;
+            Form = state.Form;
+            LastCancellationToken = new CancellationTokenSource();
+            state.OnSplit += OnSplit;
+        }
+
+        public string ComponentName => "DelaySplit";
+
+        public float HorizontalWidth => 0;
+
+        public float MinimumHeight => 0;
+
+        public float VerticalHeight => 0;
+
+        public float MinimumWidth => 0;
+
+        public float PaddingTop => 0;
+
+        public float PaddingBottom => 0;
+
+        public float PaddingLeft => 0;
+
+        public float PaddingRight => 0;
+
+        public IDictionary<string, Action> ContextMenuControls { get; protected set; }
+
+        private void OnSplit(object sender, EventArgs e)
+        {
+            LastCancellationToken.Cancel();
+            LastCancellationToken.Dispose();
+            LastCancellationToken = new CancellationTokenSource();
+            CancellationToken token = LastCancellationToken.Token;
+            String splitName = State.CurrentSplit.Name;
+            Task.Run(async () => {
+                if (splitName.Equals("auto"))
+                {
+                    await Task.Delay(5000, token);
+                    if (!token.IsCancellationRequested)
+                    {
+                        Form.BeginInvoke(new Action(() => OnSplitImpl()));
+                    }
+                }
+            });
+        }
+
+        private void OnSplitImpl()
+        {
+            Model.Split();
+        }
+
+    public void Dispose()
+        {
+
+        }
+
+        public void DrawHorizontal(System.Drawing.Graphics g, LiveSplitState state, float height, System.Drawing.Region clipRegion)
+        {
+
+        }
+
+        public void DrawVertical(System.Drawing.Graphics g, LiveSplitState state, float width, System.Drawing.Region clipRegion)
+        {
+
+        }
+
+        public XmlNode GetSettings(XmlDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Windows.Forms.Control GetSettingsControl(LayoutMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetSettings(XmlNode settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IInvalidator invalidator, LiveSplitState state, float width, float height, LayoutMode mode)
+        {
+
+        }
+    }
+}
---
+++ DelaySplitFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LiveSplit.Model;
+using LiveSplit.UI.Components;
+using LiveSplit.DelaySplit;
+
+[assembly: ComponentFactory(typeof(DelaySplitFactory))]
+
+namespace LiveSplit.DelaySplit
+{
+    public class DelaySplitFactory : IComponentFactory
+    {
+        public string ComponentName => "DelaySplit";
+
+        public string Description => "Automatically splits after a set delay.";
+
+        public ComponentCategory Category => ComponentCategory.Control;
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "";
+
+        public string UpdateURL => "";
+
+        public Version Version => GetType().Assembly.GetName().Version;
+
+        public IComponent Create(LiveSplitState state) => new DelaySplit(state);
+
+    }
+}
